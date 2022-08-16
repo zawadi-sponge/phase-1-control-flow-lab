5fe4cecd91065bbe18dfa465ceb44e0fa38c44aa
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-16 13:33:14 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+    return `This one is on me!`
+  } else if(ride > 2000 && ride <= 2500){
+    return `I will gladly take your thirty bucks.`
+  }else if(ride >  2500){
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city==="NYC"){
+    return `Ok, sounds good.`
+  }
+  else if(city!=="NYC")
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip===`generous`){
+    return 'Thank you so much.'
+  }
+  else if(tip==='not as generous'){
+    return 'Thank you.'
+  }
+  else{
+    return 'Bye.'
+  }
 }
